1858f8cf59032a89268829fb5a0a7297eff77c46
Divyanshu Kumar <93030810+Divyanshu11011@users.noreply.github.com>
2024-05-04 01:36:49 +0530

    Updated MessageGenerator.tsx with error handling

--- src/generator-modal/MessageGenerator.tsx
+++ src/generator-modal/MessageGenerator.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import { Box, Modal, Typography } from "@mui/material";
 import Generate_Icon from 'assets/modal-icons/generate-msg-icon.svg';
@@ -37,14 +38,25 @@ const MsgGeneratorModal = ({ open, handleClose }: { open: boolean; handleClose: 
 
     // Function to insert the last generated message into a LinkedIn message box
     const handleInsertMessage = () => {
-        // Finds and removes any existing placeholder in the LinkedIn message form
-        const placeHolder = document.querySelector(".msg-form__placeholder");
-        placeHolder?.remove();
         // Finds the contenteditable element where messages are typed
         const textBox = document.querySelector(".msg-form__contenteditable");
         if (textBox) {
-            // Sets the text content to the last message in the prompts array
-            textBox.textContent = messagePrompts[messagePrompts.length - 1]?.message;
+            const placeHolder = document.querySelector(".msg-form__placeholder");
+            if (placeHolder) {
+                // If a placeholder is present, remove it before inserting the message
+                placeHolder.remove();
+            }
+
+            const existingText = textBox.textContent.trim(); // Get existing text and remove leading/trailing whitespace
+
+            // If the text box is empty, overwrite the content
+            if (existingText === "") {
+                textBox.textContent = messagePrompts[messagePrompts.length - 1]?.message;
+            } else {
+                // If the text box is not empty, append the generated message
+                textBox.textContent += "\n" + messagePrompts[messagePrompts.length - 1]?.message;
+            }
+
             // Moves the cursor to the end of the text box
             const range = document.createRange();
             range.selectNodeContents(textBox);
@@ -61,6 +73,8 @@ const MsgGeneratorModal = ({ open, handleClose }: { open: boolean; handleClose: 
         handleClose();
     }
 
+
+
     return (
         <Modal
             open={open}
@@ -68,7 +82,7 @@ const MsgGeneratorModal = ({ open, handleClose }: { open: boolean; handleClose: 
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
         >
-            <Box sx={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 2, borderRadius: 2, display: 'flex', flexDirection: 'column'}}>
+            <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 2, borderRadius: 2, display: 'flex', flexDirection: 'column' }}>
                 {
                     // Maps each message prompt to a Typography component with appropriate styling
                     messagePrompts && messagePrompts.length > 0 && messagePrompts.map((prompt, index) =>
@@ -90,33 +104,33 @@ const MsgGeneratorModal = ({ open, handleClose }: { open: boolean; handleClose: 
                 {
                     // Renders buttons based on the presence of message prompts
                     messagePrompts && messagePrompts.length === 0 ?
-                    <div style={{ display: 'flex', justifyContent: "flex-end" }}>
-                        <button
-                            type="button"
-                            onClick={handleGenerateMessage}
-                            style={{ backgroundColor: "#3B82F6", color: "#fff", fontSize: "14px", fontWeight: "600", borderRadius: 4, padding: 6, marginTop: 4, display: 'flex', justifyContent: 'center', alignItems: 'center', width: "112px", cursor: "pointer" }}
-                        >
-                            <img src={Generate_Icon} alt="icon" style={{ width: "16px", height: "16px", marginRight: "8px" }} />
-                            <span style={{ textAlign: "center" }}>Generate</span>
-                        </button>
-                    </div> :
-                    <div style={{ display: 'flex', justifyContent: "flex-end", gap: 4 }}>
-                        <button
-                            type="button"
-                            onClick={handleInsertMessage}
-                            style={{ color: "#666D80", border: "2px solid #666D80", fontSize: "14px", fontWeight: "600", borderRadius: 4, padding: 6, marginTop: 4, display: 'flex', justifyContent: 'center', alignItems: 'center', width: "80px", cursor: "pointer", marginRight: 4 }}
-                        >
-                            <img src={Insert_Icon} alt="icon" style={{ width: "16px", height: "16px", marginRight: "6px" }} />
-                            <span>Insert</span>
-                        </button>
-                        <button
-                            type="button"
-                            style={{ backgroundColor: "#3B82F6", color: "#fff", fontSize: "14px", fontWeight: "600", borderRadius: 4, padding: 6, marginTop: 4, display: 'flex', justifyContent: 'center', alignItems: 'center', width: "112px", cursor: "pointer" }}
-                        >
-                            <img src={regen_icon} alt="icon" style={{ width: "16px", height: "16px", marginRight: "6px" }} />
-                            <span>Regenerate</span>
-                        </button>
-                    </div>
+                        <div style={{ display: 'flex', justifyContent: "flex-end" }}>
+                            <button
+                                type="button"
+                                onClick={handleGenerateMessage}
+                                style={{ backgroundColor: "#3B82F6", color: "#fff", fontSize: "14px", fontWeight: "600", borderRadius: 4, padding: 6, marginTop: 4, display: 'flex', justifyContent: 'center', alignItems: 'center', width: "112px", cursor: "pointer" }}
+                            >
+                                <img src={Generate_Icon} alt="icon" style={{ width: "16px", height: "16px", marginRight: "8px" }} />
+                                <span style={{ textAlign: "center" }}>Generate</span>
+                            </button>
+                        </div> :
+                        <div style={{ display: 'flex', justifyContent: "flex-end", gap: 4 }}>
+                            <button
+                                type="button"
+                                onClick={handleInsertMessage}
+                                style={{ color: "#666D80", border: "2px solid #666D80", fontSize: "14px", fontWeight: "600", borderRadius: 4, padding: 6, marginTop: 4, display: 'flex', justifyContent: 'center', alignItems: 'center', width: "80px", cursor: "pointer", marginRight: 4 }}
+                            >
+                                <img src={Insert_Icon} alt="icon" style={{ width: "16px", height: "16px", marginRight: "6px" }} />
+                                <span>Insert</span>
+                            </button>
+                            <button
+                                type="button"
+                                style={{ backgroundColor: "#3B82F6", color: "#fff", fontSize: "14px", fontWeight: "600", borderRadius: 4, padding: 6, marginTop: 4, display: 'flex', justifyContent: 'center', alignItems: 'center', width: "112px", cursor: "pointer" }}
+                            >
+                                <img src={regen_icon} alt="icon" style={{ width: "16px", height: "16px", marginRight: "6px" }} />
+                                <span>Regenerate</span>
+                            </button>
+                        </div>
                 }
             </Box>
         </Modal>
